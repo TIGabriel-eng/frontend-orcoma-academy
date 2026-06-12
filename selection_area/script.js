92bea00023e1a385c02b950948578fb58e5e539f
gabrielanacleto-maker <gabriel.anacleto.cruz@gmail.com>
2026-06-12 17:19:51 -0300

    alterações

--- selection_area/script.js
+++ selection_area/script.js
@@ -35,3 +35,59 @@ botoes.forEach(botao => {
     });
 
 });
+
+
+/* efeito pramium nos cards */
+
+const cards = document.querySelectorAll('.plano-card');
+
+cards.forEach(card => {
+
+    card.addEventListener('mousemove', (e) => {
+
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const centerX = rect.width / 3;
+        const centerY = rect.height / 3;
+
+        const rotateY = (x - centerX) / 25;
+        const rotateX = -(y - centerY) / 25;
+
+        card.style.transform =
+            `perspective(1000px)
+             rotateX(${rotateX}deg)
+             rotateY(${rotateY}deg)
+             translateY(-5px)`;
+
+    });
+
+    card.addEventListener('mouseleave', () => {
+
+        card.style.transform =
+            'perspective(1000px) rotateX(0) rotateY(0)';
+
+    });
+
+});
+
+
+/* Cards entrando com efeito premium */
+
+window.addEventListener('load', () => {
+
+    const cards = document.querySelectorAll('.plano-card');
+
+    cards.forEach((card,index) => {
+
+        setTimeout(() => {
+
+            card.classList.add('show');
+
+        }, index * 150);
+
+    });
+
+});
